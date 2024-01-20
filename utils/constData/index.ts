4377af3e5a2c0547f3e70565ac6668cd649b8eb4
shebll <shebblloll@gmail.com>
2024-01-20 19:10:09 +0200

    update prisma and api backend

--- utils/constData/index.ts
+++ utils/constData/index.ts
@@ -1,2 +1,4 @@
 export const APIBACKEND =
-  "http://ec2-13-127-192-129.ap-south-1.compute.amazonaws.com";
+  process.env.NODE_ENV == "production"
+    ? "http://flask:5000"
+    : "http://ec2-13-127-192-129.ap-south-1.compute.amazonaws.com";
